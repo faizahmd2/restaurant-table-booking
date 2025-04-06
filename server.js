7a294a22ff850f5622504bf074871b1b8f9e5938
Faiz <64007466+faizahmd2@users.noreply.github.com>
2025-04-06 10:11:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(cors(corsOptions));
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 1000,
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
   message: 'Too many requests from this IP, please try again after 15 minutes'
